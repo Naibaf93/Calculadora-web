188e086fad64953058a72b493166c09b7ba759fb
Fabián Huertas <miloh9318@hotmail.com>
2025-01-31 09:49:52 -0500

    funciones sumar, restar, multiplicar y dividir

--- js/app.js
+++ js/app.js
@@ -1,3 +1,31 @@
 function mostrarResultado(resultado) {
     document.getElementById("resultado").value = resultado;
 }
+
+function sumar() {
+    let numero1 = +document.getElementById("campo1").value;
+    let numero2 = +document.getElementById("campo2").value;
+
+    mostrarResultado(numero1 + numero2)
+}
+
+function restar() {
+    let numero1 = +document.getElementById("campo1").value;
+    let numero2 = +document.getElementById("campo2").value;
+
+    mostrarResultado(numero1 - numero2)
+}
+
+function multiplicar() {
+    let numero1 = +document.getElementById("campo1").value;
+    let numero2 = +document.getElementById("campo2").value;
+
+    mostrarResultado(numero1 * numero2)
+}
+
+function dividir() {
+    let numero1 = +document.getElementById("campo1").value;
+    let numero2 = +document.getElementById("campo2").value;
+
+    mostrarResultado(numero1 / numero2)
+}
